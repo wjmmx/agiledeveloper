90a49a67bd7a235b9bce24d715b623fe33c319e3
lsdalupan <lsdalupan@git.com>
2017-09-14 10:05:53 +0800

    dash

--- src/main/group3/Group3.java
+++ src/main/group3/Group3.java
@@ -3,5 +3,6 @@ package group3;
 public class Group3 {
   public static void main(String[] args) {
     System.out.println("Junno Beringuela");
+    System.out.println("Louise-Marie S. Dalupan");
   }
 }
