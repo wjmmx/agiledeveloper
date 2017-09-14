0203b3e39d5819595e1cf3fe0386cda6096c7cc4
Cenon Plana <nonieplana@gmail.com>
2017-09-14 10:32:49 +0800

    commit Cenon

--- src/main/group1/Group1.java
+++ src/main/group1/Group1.java
@@ -6,11 +6,8 @@ package group1;
 public class Group1 {
     public static void main(String[] args) {
         System.out.println("Ghino Punzalan");
-<<<<<<< HEAD
         System.out.println("Cenon Plana");
-=======
 		System.out.println("Doris Zhang");
->>>>>>> 19f9467276d587c1ca45c896b36a34ca98cc61e3
     }
 
 
