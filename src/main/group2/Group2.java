13fe81a8e87d918643076e06c8d68f06d1ac190b
Jag Gervacio <GERVAJO@ZHA-ITA129-W7.corp.oocl.com>
2017-09-14 10:11:54 +0800

    Added Group2 member: John Adriel Gervacio

--- src/main/group2/Group2.java
+++ src/main/group2/Group2.java
@@ -5,5 +5,6 @@ public class Group2 {
   public static void main(String[] args) {
     System.out.println("Jason Cui");
     System.out.println("Princess Hannah Bangibang");
+    System.out.println("John Adriel Gervacio");
   }
 }
