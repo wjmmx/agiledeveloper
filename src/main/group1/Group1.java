5bd5b1be78f1bdb8bdedd4267449e06f0dcb1209
Ghino Punzalan <ghino.punzalan@oocl.com>
2017-09-14 10:20:34 +0800

    Update Ghino

---
+++ src/main/group1/Group1.java
@@ -0,0 +1,11 @@
+package group1;
+
+/**
+ * Created by PUNZAGH on 9/14/2017.
+ */
+public class Group1 {
+    public static void main(String[] args) {
+        System.out.println("Ghino Punzalan");
+    }
+}
+
