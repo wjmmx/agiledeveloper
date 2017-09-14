a15c27525851c4732b40c9ef31c3bea5ea6fe019
tengkh <khimberlyjhyms.teng.oocl.com>
2017-09-14 10:35:31 +0800

    added khim

--- src/main/group1/Group1.java
+++ src/main/group1/Group1.java
@@ -7,6 +7,7 @@ public class Group1 {
     public static void main(String[] args) {
         System.out.println("Ghino Punzalan");
 		System.out.println("Doris Zhang");
+        System.out.println("Khimberlyjhyms Teng");
     }
 }
 
