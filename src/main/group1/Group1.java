a08ec376045fc613299644f194b00c517b816c7a
escobjo <jossel.escober@oocl.com>
2017-09-14 10:29:03 +0800

    Name of Jossel added

--- src/main/group1/Group1.java
+++ src/main/group1/Group1.java
@@ -5,7 +5,7 @@ package group1;
  */
 public class Group1 {
     public static void main(String[] args) {
-        System.out.println("Ghino Punzalan");
+        System.out.println("Jossel Escober");
     }
 }
 
