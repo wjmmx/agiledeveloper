ae8328e5c73e7bd986781c548f7c733cc20672a8
FoundationPig <leviemmanuelfrias@gmail.com>
2017-09-14 10:16:44 +0800

    Please accept it

--- src/main/group3/Group3.java
+++ src/main/group3/Group3.java
@@ -4,5 +4,7 @@ public class Group3 {
   public static void main(String[] args) {
     System.out.println("Junno Beringuela");
     System.out.println("Louise-Marie S. Dalupan");
+    System.out.println("Levi Emmanuel O. Frias");
+     
   }
 }
