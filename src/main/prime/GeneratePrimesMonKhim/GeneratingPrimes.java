af956c60ad8b25d2d499f3b038ce2c143f87753e
tengkh <khimberlyjhyms.teng.oocl.com>
2017-09-15 16:16:07 +0800

    changed

--- src/main/prime/GeneratePrimesMonKhim/GeneratingPrimes.java
+++ src/main/prime/GeneratePrimesMonKhim/GeneratingPrimes.java
@@ -5,40 +5,26 @@ import java.util.Arrays;
 
 public class GeneratingPrimes {
 
+	public static void main(String[] args) {
+		GeneratePrimeNumbers(2);
+	}
+
 	public static ArrayList<Integer> GeneratePrimeNumbers(int maxValue) {
 
-		ArrayList<Integer> primeNumbers = new ArrayList<Integer>();
-		if (maxValue >= 2) {
-			int size = maxValue + 1;
-			int i;
-			ArrayList<Boolean> flag = new ArrayList<Boolean>(size);
-			for (i = 0; i < size; i++) {
-				flag.add(true);
-			}
-			flag.set(0, false);
-			flag.set(1, false);
 
-			int j;
-			for (i = 2; i < Math.sqrt(size) + 1; i++) {
-				if (flag.get(i)) {
-					for (j = 2 * i; j < size; j += i) {
-						flag.set(j, false);
-					}
+		ArrayList<Integer> arrList = new ArrayList<Integer>();
+		for (int x = 2; x <= maxValue; x++) {
+			boolean flag = true;
+			for (int y = 2; y <= x - 1; y++) {
+				if (x % y == 0) {
+					flag = false;
 				}
 			}
+			if (flag)
+				arrList.add(x);
 
-			int count = 0;
-			for (i = 0; i < size; i++) {
-				if (flag.get(i))
-					count++;
-			}
-
-			for (i = 0, j = 0; i < size; i++) {
-				if (flag.get(i))
-					primeNumbers.add(i);
-			}
-			return primeNumbers;
-		} else
-			return null;
+		}
+		System.out.println(arrList);
+		return arrList;
 	}
 }
